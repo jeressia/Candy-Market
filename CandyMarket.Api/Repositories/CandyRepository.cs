19688be1dc4ef250598f17dcd00ae7bc4b0490c8
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-11-06 20:00:53 -0600

    delete works

--- CandyMarket.Api/Repositories/CandyRepository.cs
+++ CandyMarket.Api/Repositories/CandyRepository.cs
@@ -1,25 +1,54 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using CandyMarket.Api.DataModels;
 using CandyMarket.Api.Dtos;
+using Dapper;
 
 namespace CandyMarket.Api.Repositories
 {
     public class CandyRepository : ICandyRepository
     {
+        string _connectionString = "Server = localhost; Database=CandyMarket; Trusted_Connection=True;";
+
         public IEnumerable<Candy> GetAllCandy()
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                db.Open();
+
+                var allCandy = db.Query<Candy>("Select * from Candy");
+
+                return allCandy.AsList();
+            }
         }
 
         public bool AddCandy(AddCandyDto newCandy)
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"INSERT INTO [dbo].[Candy]
+                                        ([Name]
+                                        ,[CandyTypeId])
+                           VALUES
+                                        (@Name
+                                        ,@CandyTypeId)";
+
+                return db.Execute(sql, newCandy) == 1;
+            }
         }
 
-        public bool EatCandy(Guid candyIdToDelete)
+        public bool EatCandy(int candyIdToDelete)
         {
-            throw new NotImplementedException();
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"delete
+                            from Candy
+                            where [Id] = @candyIdToDelete";
+
+                return db.Execute(sql, new { candyIdToDelete }) == 1;
+            }
         }
     }
 }
